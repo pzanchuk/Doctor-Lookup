9e89cd72b11e24fa1d4f3b978b998de4df5334be
Pavel Zanchuk <buzzik@yahoo.com>
2019-03-29 15:53:46 -0700

    add search by issue

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -25,6 +25,23 @@ $(document).ready(function() {
     });
   });
 
+  $('#searh-by-issue').click(function() {
+    let name = $('#name').val();
+    $('#name').val("");
 
+    let newDoctor = new Doctor();
+    let promise = newDoctor.getDoctorByName(name);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.info-name').append(newDoctor.getName(body));
+      $('.info-address').append(newDoctor.getAddress(body));
+      $('.info-number').append(newDoctor.getNumber(body));
+      $('.info-website').append(newDoctor.getWebSite(body));
+      $('.info-accepting').append(newDoctor.isAccepting(body));
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 
 });
--- src/doctor-service.js
+++ src/doctor-service.js
@@ -2,7 +2,23 @@ export class Doctor {
   getDoctorByName(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.611%2C-122.340%2C090&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getDoctorByIssue(issue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=47.611%2C-122.340%2C090&skip=0&limit=10&user_key=40984f6d0d0c00fb3ea39c291e28d322=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -32,9 +48,6 @@ export class Doctor {
 
   getAddress(promise){
     let address = "";
-    if(promise.data.length === 0){
-      return "No results."
-    }
     for (let i = 0; i < promise.data.length; i++) {
       address +="<li>" + promise.data[i].practices[0].visit_address.street + ", " + promise.data[i].practices[0].visit_address.city + " " + promise.data[i].practices[0].visit_address.state +", " + promise.data[i].practices[0].visit_address.zip + "</li>";
     }
@@ -43,9 +56,6 @@ export class Doctor {
 
   getNumber(promise){
     let number = "";
-    if(promise.data.length === 0){
-      return "No results."
-    }
     for (let i = 0; i < promise.data.length; i++) {
       number +="<li>" + promise.data[i].practices[0].phones[0].number + "</li>";
     }
@@ -54,9 +64,6 @@ export class Doctor {
 
   getWebSite(promise){
     let website = "";
-    if(promise.data.length === 0){
-      return "No results."
-    }
     for (let i = 0; i < promise.data.length; i++) {
       website +="<li>" + promise.data[i].practices[0].website + "</li>";
     }
@@ -65,9 +72,6 @@ export class Doctor {
 
   isAccepting(promise){
     let accepting = "";
-    if(promise.data.length === 0){
-      return "No results."
-    }
     for (let i = 0; i < promise.data.length; i++) {
       if(promise.data[i].practices[0].accepts_new_patients === true){
         accepting += "<li>Yes</li>";
