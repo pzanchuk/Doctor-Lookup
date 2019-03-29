caa26e4e0b52a8c7aa397ba1d01760d458ff3660
Pavel Zanchuk <buzzik@yahoo.com>
2019-03-29 15:03:20 -0700

    add methods to pring address, phone, website and patient-acceptance status

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -15,7 +15,11 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showName').append(newDoctor.getDocsInfoByName(body));
+      $('.info-name').append(newDoctor.getName(body));
+      $('.info-address').append(newDoctor.getAddress(body));
+      $('.info-number').append(newDoctor.getNumber(body));
+      $('.info-website').append(newDoctor.getWebSite(body));
+      $('.info-accepting').append(newDoctor.isAccepting(body));
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
--- src/doctor-service.js
+++ src/doctor-service.js
@@ -15,17 +15,66 @@ export class Doctor {
     });
   }
 
-  getDocsInfoByName(promise){
-      let names = "";
-      let website ="";
-      let 
-      if(promise.data.length === 0){
-        return "No results."
-      }
-      for (let i = 0; i < promise.data.length; i++) {
-        names +="<li>" + promise.data[i].profile.first_name + " " + promise.data[i].profile.last_name + "</li>";
-      }
-      return names;
-    }
+  getName(promise){
+    let names = "";
+    // let website ="";
+    // let accepting = "";
 
+
+    if(promise.data.length === 0){
+      return "No results."
+    }
+    for (let i = 0; i < promise.data.length; i++) {
+      names +="<li>" + promise.data[i].profile.first_name + " " + promise.data[i].profile.last_name + "</li>";
+    }
+    return names;
+  }
+
+  getAddress(promise){
+    let address = "";
+    if(promise.data.length === 0){
+      return "No results."
+    }
+    for (let i = 0; i < promise.data.length; i++) {
+      address +="<li>" + promise.data[i].practices[0].visit_address.street + ", " + promise.data[i].practices[0].visit_address.city + " " + promise.data[i].practices[0].visit_address.state +", " + promise.data[i].practices[0].visit_address.zip + "</li>";
+    }
+    return address;
+  }
+
+  getNumber(promise){
+    let number = "";
+    if(promise.data.length === 0){
+      return "No results."
+    }
+    for (let i = 0; i < promise.data.length; i++) {
+      number +="<li>" + promise.data[i].practices[0].phones[0].number + "</li>";
+    }
+    return number;
+  }
+
+  getWebSite(promise){
+    let website = "";
+    if(promise.data.length === 0){
+      return "No results."
+    }
+    for (let i = 0; i < promise.data.length; i++) {
+      website +="<li>" + promise.data[i].practices[0].website + "</li>";
+    }
+    return website;
+  }
+
+  isAccepting(promise){
+    let accepting = "";
+    if(promise.data.length === 0){
+      return "No results."
+    }
+    for (let i = 0; i < promise.data.length; i++) {
+      if(promise.data[i].practices[0].accepts_new_patients === true){
+        accepting += "<li>Yes</li>";
+      }else{
+        accepting += "<li>No</li>";
+      }
+    }
+    return accepting;
+  }
 }
